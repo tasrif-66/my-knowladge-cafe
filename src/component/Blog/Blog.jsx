bffde4c1dc1b2a8fec489e1b1b37d3efac541179
tasrif-66 <mtasrif20@gmail.com>
2024-10-23 13:04:35 +0600

    dinamicaly data load

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,10 +1,25 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-  console.log(blog);
+  const { title,cover,author,author_img,post_date, reading_time,hash_tag } = blog;
   
   return (
     <div>
+      <img className='' src={cover} alt={`cover picture of title ${title}`} />
+      <div className='flex justify-between'>
+        <div className='flex gap-5 m-3'>
+          <img className='rounded-full w-14' src={author_img} alt="" />
+            <div>
+            <h3 className='text-2xl '>{author}</h3>
+            <p>{ post_date}</p>
+            </div>
+        </div>
+        <div>
+          <span>{reading_time } min read</span> 
+        </div>
+      </div>
+      <h2 className='text-4xl'>{title}</h2>
+      <p><a href="">{ hash_tag}</a></p>
       
     </div>
   );
--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,6 +1,7 @@
 import { data } from "autoprefixer";
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -15,7 +16,13 @@ const Blogs = () => {
 
   return (
     <div className="md:w-2/3">
-      <h2 className="text-4xl">Blogs {blogs.length }</h2>
+      <h2 className="text-4xl">Blogs {blogs.length}</h2>
+      {
+        blogs.map(blog => <Blog
+          key={blog.id}
+          blog={blog}
+        ></Blog>)
+      }
     </div>
   );
 };
